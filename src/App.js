8e4a47b033f0d09119fd7c36744a0287898b9ff6
hypercubist <qpinguz@gmail.com>
2021-12-10 01:46:03 +0900

    211209

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import Button from "./Button";
-
 function App() {
-  return (
-    <div>
-      <h1>Welcome</h1>
-      <Button text={"Continue"} />
-    </div>
-  );
+  return null;
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,25 @@
+import propTypes from "prop-types";
+
+function Movie({ coverImage, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImage} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  summary: propTypes.string.isRequired,
+  genres: propTypes.arrayOf(propTypes.string).isRequired,
+};
+
+export default Movie;
